c1092274b040458ad7242e509c7f06a8034c2f4e
jwala-surya-pratap <jwalasurya@outlook.com>
2026-09-06 11:45:06 +0530

    Very Rudimentary IC Implementation

--- digital_ic_simulator.py
+++ digital_ic_simulator.py
@@ -1,2 +1,26 @@
-print("Hello")
-print("Hello")
+node_relations = {}
+node_values = {}
+
+class IC:
+    inputs = []
+    outputs = []
+    def __init__(self, no_of_inputs, no_of_outputs):
+        self.no_of_inputs = no_of_inputs
+        self.no_of_outputs = no_of_outputs
+
+    def input_values(self):
+        for i in range(self.no_of_inputs):
+            entry = int(input(f"Input {i}: "))
+            self.inputs.append(entry)
+
+
+class IC7404(IC):
+    def __init__(self, no_of_inputs = 7, no_of_outputs = 7):
+        super().__init__(no_of_inputs, no_of_outputs)
+    
+    def operation(self):
+        for i in range(self.no_of_inputs):
+            self.outputs.append(not self.inputs[i])
+
+class God():
+    
