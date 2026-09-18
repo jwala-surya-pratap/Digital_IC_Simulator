f76645362aeb38ab424c072f456d27bef60c4a6b
jwala-surya-pratap <jwalasurya@outlook.com>
2026-09-18 16:54:12 +0530

    Restructuring the Manager class and python.toml file

--- src/core/manager.py
+++ src/core/manager.py
@@ -1,20 +1,20 @@
-no_of_ics = 0
+no_of_ics:int = 0
 
 class Manager():
-    node_relations = {}
-    node_values = {}
+    node_relations:dict = {}
+    node_values:dict = {}
     
-    counter = 0
+    counter:int = 0
 
     @staticmethod
-    def get_nodes(self, input_count, output_count):
-        input_nodes = []
+    def get_nodes(self, input_count:int, output_count:int) -> tuple[list[int], list[int]] :
+        input_nodes:list[int] = []
         for _ in range(input_count):
             Manager.counter += 1
             input_nodes.append(Manager.counter)
             self.node_values.update[{Manager.counter : 0}]
 
-        output_nodes = []
+        output_nodes:list[int] = []
         for _ in range(output_count):
             Manager.counter += 1
             input_nodes.append(Manager.counter)
@@ -22,7 +22,7 @@ class Manager():
 
         return (input_nodes, output_nodes)
 
-    def set_node_relations(self, other1, other2, node_index_output, node_index_input):
+    def set_node_relations(self, other1, other2, node_index_output:int, node_index_input:int):
         for i in range(other1.no_of_inputs):
             self.node_relations.update[{other2.output_nodes[node_index_output] : other1.input_nodes[node_index_input]}]
 
@@ -33,8 +33,8 @@ class Manager():
 
 
 class IC:
-    node_start_value = 0
-    def __init__(self, no_of_inputs, no_of_outputs):
+
+    def __init__(self, no_of_inputs:int, no_of_outputs:int):
         self.no_of_inputs = no_of_inputs
         self.no_of_outputs = no_of_outputs
 
