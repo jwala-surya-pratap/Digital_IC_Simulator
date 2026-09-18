500057f5d49bc4e7697bfd977b2a63f7aa435e88
jwala-surya-pratap <jwalasurya@outlook.com>
2026-09-18 14:00:54 +0530

    The Manager Class

--- digital_ic_simulator.py
+++ digital_ic_simulator.py
@@ -1,26 +1,67 @@
-node_relations = {}
-node_values = {}
+no_of_ics = 0
+
+class Manager():
+    node_relations = {}
+    node_values = {}
+    
+    counter = 0
+
+    @staticmethod
+    def get_nodes(self, input_count, output_count):
+        input_nodes = []
+        for _ in range(input_count):
+            Manager.counter += 1
+            input_nodes.append(Manager.counter)
+            self.node_values.update[{Manager.counter : 0}]
+
+        output_nodes = []
+        for _ in range(output_count):
+            Manager.counter += 1
+            input_nodes.append(Manager.counter)
+            self.node_values.update[{Manager.counter : 0}]
+
+        return (input_nodes, output_nodes)
+
+    def set_node_relations(self, other1, other2, node_index_output, node_index_input):
+        for i in range(other1.no_of_inputs):
+            self.node_relations.update[{other2.output_nodes[node_index_output] : other1.input_nodes[node_index_input]}]
+
+    def create_ic(self, ic):
+        pass
+    
+
+
 
 class IC:
-    inputs = []
-    outputs = []
+    node_start_value = 0
     def __init__(self, no_of_inputs, no_of_outputs):
         self.no_of_inputs = no_of_inputs
         self.no_of_outputs = no_of_outputs
+
+        no_of_ics += 1
+
+        self.inputs_nodes, self.outputs_nodes = Manager.get_nodes(self.no_of_inputs, self.no_of_outputs)
+
+        self.node_start_value = Manager.counter
+
 
     def input_values(self):
         for i in range(self.no_of_inputs):
             entry = int(input(f"Input {i}: "))
             self.inputs.append(entry)
 
+    
+
+
 
 class IC7404(IC):
     def __init__(self, no_of_inputs = 7, no_of_outputs = 7):
         super().__init__(no_of_inputs, no_of_outputs)
+
     
-    def operation(self):
+    def evaluate(self):
         for i in range(self.no_of_inputs):
             self.outputs.append(not self.inputs[i])
+    
 
-class God():
     
