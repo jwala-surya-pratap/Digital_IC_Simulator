7a3932fedf14acc7aa0718625c136ed0d8e78ac7
jwala-surya-pratap <jwalasurya@outlook.com>
2026-09-19 12:10:19 +0530

    Fixed formatting issues

--- src/core/ICs.py
+++ src/core/ICs.py
@@ -1,23 +1,32 @@
 from .manager import Manager
 
-no_of_ics:int = 0
+no_of_ics: int = 0
+
 
 class IC:
-    
-    def __init__(self, no_of_inputs:int, no_of_outputs:int):
+
+    def __init__(self, no_of_inputs: int, no_of_outputs: int):
         self.no_of_inputs = no_of_inputs
         self.no_of_outputs = no_of_outputs
 
         global no_of_ics
         no_of_ics += 1
 
-        self.input_nodes, self.output_nodes = Manager.get_nodes(self.no_of_inputs, self.no_of_outputs)
+        self.input_nodes, self.output_nodes = Manager.get_nodes(
+            self.no_of_inputs, self.no_of_outputs
+        )
 
         self.node_start_value = Manager.counter
-
 
     def input_values(self):
         for i in range(self.no_of_inputs):
             entry = int(input(f"Input {i}: "))
-            Manager.node_values.update({self.input_nodes[i] : entry})
+            Manager.node_values.update({self.input_nodes[i]: entry})
+
+class TestIC(IC):
+    def __init__(self):
+        super().__init__(1, 1)
+
+        super().input_values()
+
 
--- src/core/manager.py
+++ src/core/manager.py
@@ -1,35 +1,43 @@
 from .ICs import IC
 
 
+class Manager:
+    node_relations: dict[int, int] = {}
+    node_values: dict[int, int] = {}
 
-class Manager():
-    node_relations:dict[int, int] = {}
-    node_values:dict[int, int] = {}
-    
-    counter:int = 0
+    counter: int = 0
 
     @classmethod
-    def get_nodes(cls, input_count:int, output_count:int) -> tuple[list[int], list[int]] :
-        input_nodes:list[int] = []
+    def get_nodes(
+        cls, input_count: int, output_count: int
+    ) -> tuple[list[int], list[int]]:
+        input_nodes: list[int] = []
         for _ in range(input_count):
             Manager.counter += 1
             input_nodes.append(Manager.counter)
-            cls.node_values.update({Manager.counter : 0})
+            cls.node_values.update({Manager.counter: 0})
 
-        output_nodes:list[int] = []
+        output_nodes: list[int] = []
         for _ in range(output_count):
             Manager.counter += 1
             output_nodes.append(Manager.counter)
-            cls.node_values.update({Manager.counter : 0})
+            cls.node_values.update({Manager.counter: 0})
 
         return (input_nodes, output_nodes)
 
-    def set_node_relations(self, other1:"IC", other2:"IC", node_index_output:int, node_index_input:int):
+    def set_node_relations(
+        self, other1: "IC", other2: "IC", node_index_output: int, node_index_input: int
+    ):
         for _ in range(other1.no_of_inputs):
-            self.node_relations.update({other2.output_nodes[node_index_output] : other1.input_nodes[node_index_input]})
+            self.node_relations.update(
+                {
+                    other2.output_nodes[node_index_output]: other1.input_nodes[
+                        node_index_input
+                    ]
+                }
+            )
 
-    def create_ic(self, ic:str): # Its been given string for now, idk how we gonna implement the gui
+    def create_ic(
+        self, ic: str
+    ):  # Its been given string for now, idk how we gonna implement the gui
         pass
-    
-
-
